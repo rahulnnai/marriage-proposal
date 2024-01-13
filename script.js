17ca340dd858fff43d560fd86303e98a844e971f
rahulnnai <rahulnai738343@gmail.com>
2024-01-13 18:57:58 +0530

    first

---
+++ script.js
@@ -0,0 +1,31 @@
+function flyToRandom() {
+    const button = document.getElementById('no');
+
+    // Calculate random position within the viewport
+    const x = Math.random() * window.innerWidth - 10;
+    const y = Math.random() * window.innerHeight - 10;
+
+    // Set the new position
+    button.style.left = `${x}px`;
+    button.style.top = `${y}px`;
+}
+
+function clickyes() {
+    let image = document.getElementById('propimg');
+    let text = document.getElementById('text');
+    let yesbt = document.getElementById('yes');
+    let nobt = document.getElementById('no');
+    let h = document.getElementById('propimg');
+    let w = document.getElementById('propimg');
+
+    image.src = 'assets/dance.gif';
+    image.alt = 'dance-video';
+    h.style.height = '336px';
+    w.style.width = '592px';
+
+    text.innerHTML = "In your 'yes', my world found its forever.";
+
+    yesbt.parentNode.removeChild(yesbt);
+    nobt.parentNode.removeChild(nobt);
+
+}
